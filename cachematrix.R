7194c6a52c8d2ac46a0fae737d2042b290854d2b
Shreyas147 <shreyas1471999@gmail.com>
2020-06-06 01:14:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
+makeCacheMatrix <- function(x = matrix()) 
+  {
+    j <- NULL                                           ##NULL assigned to J
+    set <- function(y)                                  ##setting value of matrix 
+      {                                                 
+      x <<- y                                           ## <<- assigns a value to an object in an environment                                                   
+      j <<- NULL                                        ## that is different from the current environment                                     
+      }
+    get <- function()x                                  ##getting value of matrix
+    setInverse <- function(inverse) j <<- inverse       ##Setting value of inverse
+    getInverse <- function() j                          ##getting value of the inverse
+    list(set = set, get = get, 
+    setInverse = setInverse, 
+    getInverse = getInverse)
+  }
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+  {
+    j <- x$getInverse()                                ##returns a matrix that is inverse of x and assigned to j
+    if(!is.null(j))                                    ## if inverse of x is already calculated,it can get the 
+      {                                               ## inverse from the cache
+        message("getting cached data")
+        return(j)
+      }
+    mat <- x$get()                                    
+    j <- solve(mat,...)                                 ##for computing inverse of matrix we will use solve()function
+    x$setInverse(j)                                     ##setting the value of inverse of matrix
+    j
 }
